8543d0521b56baf8d24c31ccd16b0a76da1a8498
Harkaran4846 <harkaran4846.be23@chitkara.edu.in>
2024-08-25 00:03:39 +0530

    Update secrets.h

--- task2.1pWebHook/secrets.h
+++ task2.1pWebHook/secrets.h
@@ -1,4 +1,4 @@
-#define SECRET_SSID "realme"                         
-#define SECRET_PASS "realme221"                    
+#define SECRET_SSID "abcd"                         
+#define SECRET_PASS "12345678"                    
 #define SECRET_CH_ID  2635600                      
 #define SECRET_WRITE_APIKEY "K1DD5MNCXN46E2EU"  
